e67cf81c65e0b11b3677260b94d8e8f14d28c5c0
Igli milaqi <milaqiigli@gmail.com>
2018-12-05 22:25:04 +0100

    minor

---
+++ Project Indie Game/Assets/Scripts/MainMenu/CollectableText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableText : MonoBehaviour
+{
+	private TextMeshProUGUI m_text;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		m_text = GetComponent<TextMeshProUGUI>();
+	}
+
+	private void OnEnable()
+	{
+		m_text.text = Collectable.index + "/" + Collectable.total;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
