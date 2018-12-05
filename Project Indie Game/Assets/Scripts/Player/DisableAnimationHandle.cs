7921e62ea2ade81d19d61c433cb89b9be0504545
Igli milaqi <milaqiigli@gmail.com>
2018-12-05 22:56:32 +0100

    Implemented Resolution screen

---
+++ Project Indie Game/Assets/Scripts/Player/DisableAnimationHandle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAnimationHandle : MonoBehaviour
+{
+
+	private DisablePlayer m_disablePlayer;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_disablePlayer = GetComponentInParent<DisablePlayer>();
+		m_disablePlayer.SetModel(gameObject);
+	}
+
+	public void DisablePlayer()
+	{
+		m_disablePlayer.Disableplayer();
+	}
+}
---
+++ Project Indie Game/Assets/Scripts/Player/DisablePlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePlayer : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject m_resolutionScreen;
+	
+	private GameObject m_model;
+
+	private Player m_player;
+
+	private CompanionController m_companionController;
+
+	private CompanionManager m_companionManager;
+
+	private Health m_health;
+
+	private float m_initMoveSpeed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_player = GetComponent<Player>();
+		m_companionController = GetComponent<CompanionController>();
+		m_companionManager = GetComponent<CompanionManager>();
+		m_health = GetComponent<Health>();
+
+		m_initMoveSpeed = m_player.MoveSpeed;
+	}
+
+	public void SetModel(GameObject model)
+	{
+		m_model = model;
+	}
+	
+	public void Disableplayer()
+	{
+		if (m_health.GetCurrentHealth <= 0.1f)
+		{
+			m_resolutionScreen.SetActive(true);
+		}
+		
+		m_model.SetActive(false);
+		m_player.MoveSpeed = 0;
+		m_companionController.enabled = false;
+		m_companionManager.enabled = false;
+		m_health.CanTakeDamage = false;
+	}
+
+	public void ActivatePlayer()
+	{
+		m_model.SetActive(true);
+		m_player.MoveSpeed = m_initMoveSpeed;
+		m_companionController.enabled = true;
+		m_companionManager.enabled = true;
+		m_health.CanTakeDamage = true;
+	}
+}
