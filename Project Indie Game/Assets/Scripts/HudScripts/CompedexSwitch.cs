2abe53704fb44aa6c5c3a54f9f6c88e22555ecec
August <patrickpanda7@gmail.com>
2018-12-05 20:28:06 +0100

    fixed compadex

--- Project Indie Game/Assets/Scripts/HudScripts/CompedexSwitch.cs
+++ Project Indie Game/Assets/Scripts/HudScripts/CompedexSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CompedexSwitch : MonoBehaviour
 {
@@ -9,20 +10,44 @@ public class CompedexSwitch : MonoBehaviour
     GameObject m_notes;
     [SerializeField]
     GameObject m_companions;
+    [Header("BUTTONS")]
+    [SerializeField]
+    Button m_button_notes;
+    [SerializeField]
+    Button m_button_compadex;
 
     void Start()
     {
         m_companions.SetActive(false);
         m_notes.SetActive(true);
+        m_button_notes.onClick.AddListener(SwitchToNotes);
+        m_button_compadex.onClick.AddListener(SwitchToCompadex);
     }
 
-    // Update is called once per frame
+    public void SwitchToNotes()
+    {
+        m_notes.SetActive(true);
+        m_companions.SetActive(false);
+    }
+
+    public void SwitchToCompadex()
+    {
+        m_notes.SetActive(false);
+        m_companions.SetActive(true);
+    }
+
+    public void SwitchTabs()
+    {
+        m_notes.SetActive(!m_notes.active);
+        m_companions.SetActive(!m_companions.active);
+    }
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            m_notes.SetActive(!m_notes.active);
-            m_companions.SetActive(!m_companions.active);
+            SwitchTabs();
         }
     }
 }
