a1a9d66dc327d3d433dc269273e25d950caec200
August <patrickpanda7@gmail.com>
2018-12-06 02:29:17 +0100

    Added rotation of Collectable

---
+++ Project Indie Game/Assets/Scripts/General/CollectableBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speedOfRotation=10;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Rotate(new Vector3(0, m_speedOfRotation*Time.deltaTime, 0));
+	}
+}
