dc8924b80ceb73a8234456e24229b88322c825d3
August <patrickpanda7@gmail.com>
2018-12-06 05:23:26 +0100

    major

---
+++ Project Indie Game/Assets/EntranceCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EntranceCollider : MonoBehaviour {
+
+    [SerializeField]
+    private UnityEvent OnEnter;
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnEnter.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
