032eb8c9f40f6955f2c54b8ecc947bc89e7c9e65
August <patrickpanda7@gmail.com>
2018-12-06 06:26:19 +0100

    audiomanager + etc

---
+++ Project Indie Game/Assets/GoombaAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaAnim : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource m_audioSource;
+	void Start () {
+		
+	}
+
+    public void PlaySound()
+    {
+        m_audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
